922e0a7c32d24a3aa05144259c6aaa66f9e87442
radhiprabhu <56896980+radhiprabhu@users.noreply.github.com>
2025-06-16 14:08:50 -0400

    youtubers analysis.sql

--- PowerBI/YouTubers Analytics/youtubers analysis.sql
+++ PowerBI/YouTubers Analytics/youtubers analysis.sql
@@ -15,22 +15,55 @@ CREATE VIEW view_uk_youtubers_2024 AS
     total_videos,
   FROM top_uk_youtubers_2024
 
+/*
 
+/*
 
+# Data quality tests 
 
+1. The data needs to be 100 records of YouTube channels (row count test)	
+2. The data needs 4 fields (column count test)								
+3. The channel name column must be string format, and the other columns must be numerical data types (data type check)  
+4. Each record must be unique in the dataset (duplicate count check) 
+*/	
+# 1. Row count check 
+Count the total number of records (or rows) are in the SQL view
+*/
 
+SELECT
+    COUNT(*) AS no_of_rows
+FROM
+    view_uk_youtubers_2024;
 
+/*
+# 3. Data type check
+Check the data types of each column from the view by checking the INFORMATION SCHEMA view
+*/
 
-
-
-
-
-
-
-
+SELECT
+    COLUMN_NAME,
+    DATA_TYPE
+FROM
+    INFORMATION_SCHEMA.COLUMNS
+WHERE
+    TABLE_NAME = 'view_uk_youtubers_2024';
 
 
 /*
+# 4. Duplicate records check
+-- 1. Check for duplicate rows in the view
+-- 2. Group by the channel name
+-- 3. Filter for groups with more than one row
+*/
+
+SELECT channel_name, COUNT(*) AS duplicate_count
+FROM
+    view_uk_youtubers_2024
+GROUP BY channel_name
+HAVING COUNT(*) > 1;
+
+/*
+YOUTUBERS WITH THE MOST SUBSCRIBER
 # 1. Define variables Add commentMore actions
 # 2. Create a CTE that rounds the average views per video 
 # 3. Select the column you need and create calculated columns from existing ones 
@@ -38,13 +71,10 @@ CREATE VIEW view_uk_youtubers_2024 AS
 # 5. Sort results by net profits (from highest to lowest)
 */
 
--- 1. Add commentMore actions
 DECLARE @conversionRate FLOAT = 0.02;		-- The conversion rate @ 2%
 DECLARE @productCost FLOAT = 5.0;			-- The product cost @ $5
 DECLARE @campaignCost FLOAT = 50000.0;		-- The campaign cost @ $50,000	
 
-
--- 2.  
 WITH ChannelData AS (
     SELECT 
         channel_name,
@@ -54,8 +84,6 @@ WITH ChannelData AS (
     FROM 
         youtube_db.dbo.view_uk_youtubers_2024
 )
-
--- 3. Add commentMore actions
 SELECT 
     channel_name,
     rounded_avg_views_per_video,
@@ -64,13 +92,78 @@ SELECT
     ((rounded_avg_views_per_video * @conversionRate * @productCost) - @campaignCost) AS net_profit
 FROM 
     ChannelData
-
-
--- 4. 
 WHERE 
     channel_name in ('NoCopyrightSounds', 'DanTDM', 'Dan Rhodes')    
-
-
--- 5.  
 ORDER BY
-	net_profit DESC
+	net_profit DESC;
+
+/* 
+YOUTUBERS WITH THE MOST VIDEOS
+# 1. Define variables
+# 2. Create a CTE that rounds the average views per video
+# 3. Select the columns you need and create calculated columns from existing ones
+# 4. Filter results by YouTube channels
+# 5. Sort results by net profits (from highest to lowest)
+*/
+
+DECLARE @conversionRate FLOAT = 0.02;           -- The conversion rate @ 2%
+DECLARE @productCost FLOAT = 5.0;               -- The product cost @ $5
+DECLARE @campaignCostPerVideo FLOAT = 5000.0;   -- The campaign cost per video @ $5,000
+DECLARE @numberOfVideos INT = 11;               -- The number of videos (11)
+
+WITH ChannelData AS (
+    SELECT
+        channel_name,
+        total_views,
+        total_videos,
+        ROUND((CAST(total_views AS FLOAT) / total_videos), -4) AS rounded_avg_views_per_video
+    FROM
+        youtube_db.dbo.view_uk_youtubers_2024
+)
+SELECT
+    channel_name,
+    rounded_avg_views_per_video,
+    (rounded_avg_views_per_video * @conversionRate) AS potential_units_sold_per_video,
+    (rounded_avg_views_per_video * @conversionRate * @productCost) AS potential_revenue_per_video,
+    ((rounded_avg_views_per_video * @conversionRate * @productCost) - (@campaignCostPerVideo * @numberOfVideos)) AS net_profit
+FROM
+    ChannelData
+WHERE
+    channel_name IN ('GRM Daily', 'Man City', 'YOGSCAST Lewis & Simon ')
+ORDER BY
+    net_profit DESC;
+
+/*
+YOUTUBERS WITH THE MOST VIEWS
+# 1. Define variables
+# 2. Create a CTE that rounds the average views per video
+# 3. Select the columns you need and create calculated columns from existing ones
+# 4. Filter results by YouTube channels
+# 5. Sort results by net profits (from highest to lowest)
+*/
+
+DECLARE @conversionRate FLOAT = 0.02;        -- The conversion rate @ 2%
+DECLARE @productCost MONEY = 5.0;            -- The product cost @ $5
+DECLARE @campaignCost MONEY = 130000.0;      -- The campaign cost @ $130,000
+
+WITH ChannelData AS (
+    SELECT
+        channel_name,
+        total_views,
+        total_videos,
+        ROUND(CAST(total_views AS FLOAT) / total_videos, -4) AS avg_views_per_video
+    FROM
+        youtube_db.dbo.view_uk_youtubers_2024
+)
+SELECT
+    channel_name,
+    avg_views_per_video,
+    (avg_views_per_video * @conversionRate) AS potential_units_sold_per_video,
+    (avg_views_per_video * @conversionRate * @productCost) AS potential_revenue_per_video,
+    (avg_views_per_video * @conversionRate * @productCost) - @campaignCost AS net_profit
+FROM
+    ChannelData
+WHERE
+    channel_name IN ('Mister Max', 'DanTDM', 'Dan Rhodes')
+ORDER BY
+    net_profit DESC;
